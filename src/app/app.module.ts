cb0ce49d6eda91b8484942e90a4c288d1bb861fa
Irena Jurasek <irenaskydive@gmail.com>
2022-01-22 12:46:15 +0100

    added components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule
